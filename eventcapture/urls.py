38bac9e0ca0e068f57da827c186eca829c71be3d
Tony Florida <flooreeda@gmail.com>
2018-07-01 20:17:04 -0400

    Added an event page while leveraging Django template inheritance

--- eventcapture/urls.py
+++ eventcapture/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('event', views.event, name='event'),
 ]
--- eventcapture/views.py
+++ eventcapture/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+def event(request):
+	return render(request, 'event.html')
