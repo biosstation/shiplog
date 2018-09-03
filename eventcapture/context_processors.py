d2ea85c92efda5e1ebcc4201bb534cdb2f40d095
Tony Florida <flooreeda@gmail.com>
2018-09-03 11:33:57 -0300

    Better error message with header and message body

--- eventcapture/context_processors.py
+++ eventcapture/context_processors.py
@@ -6,4 +6,5 @@ def active_cruise(request):
         context['cruise'] = Cruise.get_active_cruise()
     except ValueError as e:
         context['error'] = e
+        context['message'] = "More than one cruise has been configure to occur right now"
     return context
